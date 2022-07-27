01fd21afd89973fdc8935493cce0679cecd07f48
Aayush Krishns <krishna.aayush@gmail.com>
2022-07-27 12:56:44 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");  //For capitalizing
+const session= require("express-session");
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+const passportlocalmongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findOrCreate")
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static(__dirname))
+mongoose.connect("mongodb://localhost:27017/loginDB");
 
-//enable express to parse URL-encoded body i.e. info from HTML form
-app.use(express.urlencoded({ extended: true }));
+const signupSchema={
+  fname:String,
+  email: String,
+  password: String,
+  cpassword: String,
+  mno: Number
+};
 
-//enable express to access static files in folder called "public"
-app.use(express.static("public"));
+const signup = mongoose.model("login",signupSchema);
 
-//mongoose.connect("mongodb://127.0.0.1:27017/badmitonDB", { useNewUrlParser: true });
+const loginSchema ={
+  email: String,
+  password: String
+};
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-    console.log("Server has been initiated on port " + PORT);
+const login = mongoose.model("user",loginSchema);
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/login-signup.html");
+});
+
+app.post("/signup",function(req,res){
+  const fname = req.body.signup_fname;
+  const ema = req.body.signup_email;
+  const pass = req.body.signup_pass;
+  const cpass = req.body.signup_cpass;
+  const mno = req.body.signup_mno;
+  const newitem  = new signup({
+    fname: fname,
+    email: ema,
+    password:pass,
+    cpassword: cpass,
+    mno: mno
+  });
+  newitem.save();
+  res.sendFile(__dirname+"/login-signup.html");
+});
+
+app.post("/login",function(req,res){
+  const username = req.body.login_email;
+  const password =req.body.login_password;
+  signup.findOne({email:username},function(err,found){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(found){
+        if(found.password===password){
+            res.sendFile(__dirname+"/home.html");
+        }
+      }
+    }
+  })
+})
+
+
+
+app.listen(3000,function(){
+  console.log("server active");
 });
